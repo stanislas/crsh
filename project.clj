667fec42d53444ea3cfba79b3184ca04d60e805b
Stanislas Nanchen <stan@hood.ch>
2015-05-10 16:15:20 +0200

    fix crsh executable.
    Signed-off-by: Stanislas Nanchen <stan@hood.ch>

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.fusesource.jansi/jansi "1.11"]
-                 [org.tobereplaced/nio.file "0.4.0"]]
+                 [org.tobereplaced/nio.file "0.4.0"]
+                 [clj-http "1.1.2"]]
   :main clojure.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
